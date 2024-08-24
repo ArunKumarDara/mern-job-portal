37ac291891b61ac6d633669111f6eccc6c21547c
Arun Kumar <mymailarun32@gmail.com>
2024-08-25 05:18:15 +0530

    feat(models): create schema using mongoose

---
+++ server/models/companyModel.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const companySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    website: {
+      type: String,
+    },
+    location: {
+      type: String,
+    },
+    employees: {
+      type: Number,
+      required: true,
+    },
+    logo: {
+      type: String,
+      default: "",
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Company", companySchema);
---
+++ server/models/jobModel.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const jobSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    company: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Company",
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    requirements: [{ type: String }],
+    location: {
+      type: String,
+      required: true,
+    },
+    salary: {
+      type: Number,
+      required: true,
+    },
+    jobType: {
+      type: String,
+      required: true,
+    },
+    positions: {
+      type: Number,
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    applications: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "Application" },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Job", jobSchema);

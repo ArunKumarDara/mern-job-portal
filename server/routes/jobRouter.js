201f66ee28a30a8bee266c3f6f4d245f5d0f0627
Arun Kumar <mymailarun32@gmail.com>
2024-08-25 13:09:54 +0530

    feat(jobRouter):create router for job

---
+++ server/routes/jobRouter.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const {
+  addJob,
+  getAllJobs,
+  getJobById,
+  getAdminJobs,
+} = require("../controllers/jobController");
+const validateJwtToken = require("../middlewares/authentication");
+
+const jobRouter = express.Router();
+
+jobRouter.post("/add", validateJwtToken, addJob);
+jobRouter.get("/getAllJobs", validateJwtToken, getAllJobs);
+jobRouter.get("/getAdminJobs", validateJwtToken, getAdminJobs);
+jobRouter.get("/:id", validateJwtToken, getJobById);
+
+module.exports = jobRouter;

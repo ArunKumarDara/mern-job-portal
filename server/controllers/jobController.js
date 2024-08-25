96e358425a698ee9cebb6db094015155eaf50e7b
Arun Kumar <mymailarun32@gmail.com>
2024-08-25 13:09:13 +0530

    feat(jobController): implement crud operations functionality

---
+++ server/controllers/jobController.js
@@ -0,0 +1,120 @@
+const jobModel = require("../models/jobModel");
+
+const addJob = async (req, res) => {
+  try {
+    const {
+      title,
+      description,
+      requirements,
+      salary,
+      location,
+      jobType,
+      positions,
+      experienceLevel,
+      companyId,
+    } = req.body;
+    const userId = req.id;
+    if (
+      !title ||
+      !description ||
+      !requirements ||
+      !salary ||
+      !location ||
+      !jobType ||
+      !positions ||
+      !experienceLevel ||
+      !companyId
+    ) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Some fields are missing" });
+    }
+    const newJob = new jobModel({
+      title,
+      description,
+      requirements: requirements.split(","),
+      salary: Number(salary),
+      location,
+      jobType,
+      positions,
+      experienceLevel,
+      company: companyId,
+      createdBy: userId,
+    });
+    const response = await newJob.save();
+    return res.status(200).json({
+      success: true,
+      message: "Job added successfully",
+      data: response,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+
+const getAllJobs = async (req, res) => {
+  try {
+    const keyword = req.query.keyword || "";
+    const query = {
+      $or: [
+        { title: { $regex: keyword, $options: "i" } },
+        { description: { $regex: keyword, $options: "i" } },
+      ],
+    };
+    const jobs = await jobModel
+      .find(query)
+      .populate("company")
+      .sort({ createdAt: -1 });
+    return res.status(200).json({
+      success: true,
+      message: "jobs fetched successfully",
+      data: jobs,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+
+const getJobById = async (req, res) => {
+  try {
+    const jobId = req.params.id;
+    const job = await jobModel.findById(jobId).populate("company");
+    if (!job) {
+      return res.status(404).json({ success: false, message: "Job not found" });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "Job fetched successfully",
+      data: job,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+
+const getAdminJobs = async (req, res) => {
+  try {
+    const userId = req.id;
+    const jobs = await jobModel
+      .find({ createdBy: userId })
+      .populate("company")
+      .sort({ createdAt: -1 });
+    if (!jobs) {
+      return res.status(404).json({ success: false, message: "No jobs found" });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "Admin jobs fetched successfully",
+      data: jobs,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+
+module.exports = {
+  addJob,
+  getAllJobs,
+  getJobById,
+  getAdminJobs,
+};

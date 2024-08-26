2ab22a380a80e00f7e209d2f81b71da4a1bffdfe
Arun Kumar <mymailarun32@gmail.com>
2024-08-26 15:31:48 +0530

    feat(applicationRouter): create router for application

---
+++ server/routes/applicationRouter.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const {
+  applyJob,
+  getAppliedJobs,
+  getApplicants,
+  updateStatus,
+} = require("../controllers/applicationController");
+const validateJwtToken = require("../middlewares/authentication");
+const applicationRouter = express.Router();
+
+applicationRouter.post("/apply/:id", validateJwtToken, applyJob);
+applicationRouter.get("/get", validateJwtToken, getAppliedJobs);
+applicationRouter.get("/get/:id", validateJwtToken, getApplicants);
+applicationRouter.post("/status/:id/update", validateJwtToken, updateStatus);
+
+module.exports = applicationRouter;

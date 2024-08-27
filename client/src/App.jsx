5e400da56ff6dba3836bc4ba2e0bf3aac41855d9
Arun Kumar <mymailarun32@gmail.com>
2024-08-27 07:31:10 +0530

    feat(App): create app router using react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,56 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Job from "./pages/Job";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Navbar from "./components/Navbar";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Navbar />
+        <Home />
+      </>
+    ),
+  },
+  {
+    path: "/jobs",
+    element: (
+      <>
+        <Navbar />
+        <Job />
+      </>
+    ),
+  },
+  {
+    path: "/login",
+    element: (
+      <>
+        <Navbar />
+        <Login />
+      </>
+    ),
+  },
+  {
+    path: "/signup",
+    element: (
+      <>
+        <Navbar />
+        <Signup />
+      </>
+    ),
+  },
+]);
 
 function App() {
-  return <div className="text-orange-500">lets build job portal</div>;
+  return (
+    <>
+      <RouterProvider router={appRouter} />
+    </>
+  );
 }
 
 export default App;

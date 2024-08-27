f10cb38fa95a0fd6aac1b0ee75ef8a630a119df8
Arun Kumar <mymailarun32@gmail.com>
2024-08-27 10:49:34 +0530

    feat(login): create login page

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,73 @@
+import { Card, Form, Input, Select, Typography } from "antd";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const onFinish = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <div className="flex justify-center items-center">
+      <Card size="small" className="w-1/3">
+        <Typography.Title level={4} className="text-center">
+          Log In
+        </Typography.Title>
+        <Form layout="vertical" onFinish={onFinish}>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: "please enter your email" }]}
+          >
+            <Input size="large" />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "please enter password" }]}
+          >
+            <Input.Password size="large" />
+          </Form.Item>
+          <Form.Item
+            label="Role"
+            name="role"
+            rules={[{ required: true, message: "Please Select your Role" }]}
+          >
+            <Select
+              placement="topRight"
+              size="large"
+              placeholder="Select your Role"
+              options={[
+                {
+                  value: "student",
+                  label: "Student",
+                },
+                {
+                  value: "recruiter",
+                  label: "Recruiter",
+                },
+              ]}
+            />
+          </Form.Item>
+          <Form.Item>
+            <div>
+              <button
+                type="submit"
+                className="w-full font-semibold text-white bg-[#6A38C2] rounded-md mb-3 p-3 hover:shadow-md"
+              >
+                LOG IN
+              </button>
+              <Link to="/signup">
+                <Typography.Text className="text-center ">
+                  Don&apos;t have an account?
+                  <span className="hover:text-blue-500">Sign Up</span>
+                </Typography.Text>
+              </Link>
+            </div>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
+};
+
+export default Login;

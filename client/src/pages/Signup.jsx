6d249e19b13e3d8f5c16b99f95a5d9c87d957274
Arun Kumar <mymailarun32@gmail.com>
2024-08-29 07:24:20 +0530

    feat(sihnup): create signup form using drawer antd component

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,104 +1,90 @@
-import { Card, Form, Input, message, Select, Typography } from "antd";
-import { Link, useNavigate } from "react-router-dom";
+/* eslint-disable react/prop-types */
+import { Form, Input, message, Select } from "antd";
+import { useNavigate } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
 import { registerUser } from "../apiCalls/user";
 
-const Signup = () => {
+const Signup = ({ setSignupDrawer }) => {
   const navigate = useNavigate();
 
   const mutation = useMutation({
     mutationFn: registerUser,
     onSuccess: (data) => {
+      setSignupDrawer(false);
       message.success(data.message);
       navigate("/login");
     },
-    onError: (error) => {
-      message.error(error.message);
+    onError: (data) => {
+      message.error(data.message);
     },
   });
 
   const onFinish = (values) => {
-    console.log(values);
     mutation.mutate(values);
   };
 
   return (
-    <div className="flex justify-center items-center">
-      <Card size="small" className="w-1/3">
-        <Typography.Title level={4} className="text-center">
-          Sign Up
-        </Typography.Title>
-        <Form layout="vertical" onFinish={onFinish}>
-          <Form.Item
-            label="Full Name"
-            name="fullName"
-            rules={[{ required: true, message: "please enter your name" }]}
+    <Form layout="vertical" onFinish={onFinish}>
+      <Form.Item
+        label="Full Name"
+        name="fullName"
+        rules={[{ required: true, message: "please enter your name" }]}
+      >
+        <Input size="large" />
+      </Form.Item>
+      <Form.Item
+        label="Email"
+        name="email"
+        rules={[{ required: true, message: "please enter your email" }]}
+      >
+        <Input size="large" />
+      </Form.Item>
+      <Form.Item
+        label="Phone Number"
+        name="phoneNumber"
+        rules={[{ required: true, message: "please enter your phone number" }]}
+      >
+        <Input size="large" />
+      </Form.Item>
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: "please enter password" }]}
+      >
+        <Input.Password size="large" />
+      </Form.Item>
+      <Form.Item
+        label="Role"
+        name="role"
+        rules={[{ required: true, message: "Please Select your Role" }]}
+      >
+        <Select
+          placement="topRight"
+          size="large"
+          placeholder="Select your Role"
+          options={[
+            {
+              value: "student",
+              label: "Student",
+            },
+            {
+              value: "recruiter",
+              label: "Recruiter",
+            },
+          ]}
+        />
+      </Form.Item>
+      <Form.Item>
+        <div>
+          <button
+            type="submit"
+            className="w-full font-semibold text-white bg-[#6A38C2] rounded-md mb-3 p-3 hover:shadow-md"
           >
-            <Input size="large" />
-          </Form.Item>
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[{ required: true, message: "please enter your email" }]}
-          >
-            <Input size="large" />
-          </Form.Item>
-          <Form.Item
-            label="Phone Number"
-            name="phoneNumber"
-            rules={[
-              { required: true, message: "please enter your phone number" },
-            ]}
-          >
-            <Input size="large" />
-          </Form.Item>
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: "please enter password" }]}
-          >
-            <Input.Password size="large" />
-          </Form.Item>
-          <Form.Item
-            label="Role"
-            name="role"
-            rules={[{ required: true, message: "Please Select your Role" }]}
-          >
-            <Select
-              placement="topRight"
-              size="large"
-              placeholder="Select your Role"
-              options={[
-                {
-                  value: "student",
-                  label: "Student",
-                },
-                {
-                  value: "recruiter",
-                  label: "Recruiter",
-                },
-              ]}
-            />
-          </Form.Item>
-          <Form.Item>
-            <div>
-              <button
-                type="submit"
-                className="w-full font-semibold text-white bg-[#6A38C2] rounded-md mb-3 p-3 hover:shadow-md"
-              >
-                SIGN UP
-              </button>
-              <Link to="/login">
-                <Typography.Text>
-                  Already have an account?
-                  <span className="hover:text-blue-500">Log in</span>
-                </Typography.Text>
-              </Link>
-            </div>
-          </Form.Item>
-        </Form>
-      </Card>
-    </div>
+            SIGN UP
+          </button>
+        </div>
+      </Form.Item>
+    </Form>
   );
 };
 

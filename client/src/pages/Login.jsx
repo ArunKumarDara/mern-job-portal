04d2cc1e4a68bd6a1ae9b85411d01c4eb8a3d49f
Arun Kumar <mymailarun32@gmail.com>
2024-08-29 07:23:52 +0530

    feat(Login): reate login page using drawer antd component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,16 +1,23 @@
-import { Card, Form, Input, Select, Typography, message } from "antd";
+/* eslint-disable no-unused-vars */
+import { Form, Input, Select, Typography, message } from "antd";
 import { Link, useNavigate } from "react-router-dom";
 import { loginUser } from "../apiCalls/user";
 import { useMutation } from "@tanstack/react-query";
+import { useDispatch } from "react-redux";
+import { setUser } from "../store/userSlice";
 
-const Login = () => {
+// eslint-disable-next-line react/prop-types
+const Login = ({ setLoginDrawer }) => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const mutation = useMutation({
     mutationFn: loginUser,
     onSuccess: (data) => {
       if (data.success) {
+        setLoginDrawer(false);
         message.success(data.message);
+        dispatch(setUser(data.user));
         navigate("/");
       } else {
         message.error(data.message);
@@ -26,66 +33,55 @@ const Login = () => {
   };
 
   return (
-    <div className="flex justify-center items-center">
-      <Card size="small" className="w-1/3">
-        <Typography.Title level={4} className="text-center">
-          Log In
-        </Typography.Title>
-        <Form layout="vertical" onFinish={onFinish}>
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[{ required: true, message: "please enter your email" }]}
-          >
-            <Input size="large" />
-          </Form.Item>
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: "please enter password" }]}
-          >
-            <Input.Password size="large" />
-          </Form.Item>
-          <Form.Item
-            label="Role"
-            name="role"
-            rules={[{ required: true, message: "Please Select your Role" }]}
-          >
-            <Select
-              placement="topRight"
-              size="large"
-              placeholder="Select your Role"
-              options={[
-                {
-                  value: "student",
-                  label: "Student",
-                },
-                {
-                  value: "recruiter",
-                  label: "Recruiter",
-                },
-              ]}
-            />
-          </Form.Item>
-          <Form.Item>
-            <div>
-              <button
-                type="submit"
-                className="w-full font-semibold text-white bg-[#6A38C2] rounded-md mb-3 p-3 hover:shadow-md"
-              >
-                LOG IN
-              </button>
-              <Link to="/signup">
-                <Typography.Text className="text-center ">
-                  Don&apos;t have an account?
-                  <span className="hover:text-blue-500">Sign Up</span>
-                </Typography.Text>
-              </Link>
-            </div>
-          </Form.Item>
-        </Form>
-      </Card>
-    </div>
+    <>
+      <Form layout="vertical" onFinish={onFinish}>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[{ required: true, message: "please enter your email" }]}
+        >
+          <Input size="large" />
+        </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "please enter password" }]}
+        >
+          <Input.Password size="large" />
+        </Form.Item>
+        <Form.Item
+          label="Role"
+          name="role"
+          rules={[{ required: true, message: "Please Select your Role" }]}
+        >
+          <Select
+            placement="topRight"
+            size="large"
+            placeholder="Select your Role"
+            options={[
+              {
+                value: "student",
+                label: "Student",
+              },
+              {
+                value: "recruiter",
+                label: "Recruiter",
+              },
+            ]}
+          />
+        </Form.Item>
+        <Form.Item>
+          <div>
+            <button
+              type="submit"
+              className="w-full font-semibold text-white bg-[#6A38C2] rounded-md mb-3 p-3 hover:shadow-md"
+            >
+              LOG IN
+            </button>
+          </div>
+        </Form.Item>
+      </Form>
+    </>
   );
 };
 

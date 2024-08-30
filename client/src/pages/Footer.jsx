611a35341b9a6a62dbb4a273b6dc57ff0acfc7a0
Arun Kumar <mymailarun32@gmail.com>
2024-08-30 22:49:49 +0530

    fix(footer): change padding size

--- client/src/pages/Footer.jsx
+++ client/src/pages/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="border-t border-t-gray-200 py-6">
-      <div className="container mx-auto px-4">
+      <div className="container mx-auto px-8">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
             <h2 className="text-xl font-bold">Job Hunt</h2>

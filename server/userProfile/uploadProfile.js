923c3337c3634e374e0bab1ec6c55a6d6a70dfcf
Arun Kumar <mymailarun32@gmail.com>
2024-08-30 17:30:14 +0530

    feat(uploadProfile): upload files using multer middleware

---
+++ server/userProfile/uploadProfile.js
@@ -0,0 +1,20 @@
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    console.log(file);
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    console.log(file);
+    const random = uuidv4();
+    cb(null, random + "" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+const multerUploader = upload.single("profilePic");
+
+module.exports = multerUploader;

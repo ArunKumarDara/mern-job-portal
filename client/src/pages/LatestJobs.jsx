aec49222c362428dc96ca88542e32fbc02633d82
Arun Kumar <mymailarun32@gmail.com>
2024-08-31 02:45:25 +0530

    feat(latestJobs): show when job created from now and fix loading error

--- client/src/pages/LatestJobs.jsx
+++ client/src/pages/LatestJobs.jsx
@@ -1,13 +1,14 @@
-import { Card, List, Spin, Tag } from "antd";
+import { Card, List, Tag } from "antd";
 import { useQuery } from "@tanstack/react-query";
 import { getLatestJobs } from "../apiCalls/job";
 import {
-  LoadingOutlined,
   PushpinOutlined,
   EnvironmentOutlined,
   HomeOutlined,
 } from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
+import Loading from "../components/Loading";
+import moment from "moment";
 
 const LatestJobs = () => {
   const navigate = useNavigate();
@@ -20,59 +21,64 @@ const LatestJobs = () => {
     queryFn: getLatestJobs,
   });
 
-  if (isLoading)
-    return <Spin indicator={<LoadingOutlined spin />} size="large" />;
-
   if (error) return <p>Error fetching latest jobs: {error.message}</p>;
+
+  const now = moment();
 
   return (
     <div>
-      <h1 className="text-2xl md:text-4xl font-bold mt-5 mb-5">
+      <h1 className="text-xl md:text-2xl font-bold mt-5 mb-5">
         Latest & Top<span className="text-[#6A38C2]"> Job Openings</span>
       </h1>
-      <List
-        grid={{
-          gutter: 12,
-          xs: 1,
-          sm: 2,
-          md: 2,
-          lg: 2,
-          xl: 2,
-          xxl: 3,
-        }}
-        dataSource={latestJobs.data.slice(0, 6)}
-        renderItem={(job) => (
-          <List.Item>
-            <Card
-              className="cursor-pointer"
-              hoverable
-              onClick={() => navigate(`/jobs/${job._id}`)}
-            >
-              <div className="flex justify-between items-start">
-                <div>
-                  <h1 className="font-medium text-lg">{job.title}</h1>
-                  <p className="text-sm">
-                    <HomeOutlined className="mr-2" />
-                    {job.company.name}
-                  </p>
-                  <p className="text-sm text-gray-500">
-                    <EnvironmentOutlined className="mr-2" />
-                    {job.location}
-                  </p>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <List
+          grid={{
+            gutter: 12,
+            xs: 1,
+            sm: 2,
+            md: 2,
+            lg: 2,
+            xl: 2,
+            xxl: 3,
+          }}
+          dataSource={latestJobs.data.slice(0, 6)}
+          renderItem={(job) => (
+            <List.Item>
+              <Card
+                className="cursor-pointer"
+                hoverable
+                onClick={() => navigate(`/jobs/${job._id}`)}
+              >
+                <div className="flex justify-between items-start">
+                  <div>
+                    <h1 className="font-medium text-lg">{job.title}</h1>
+                    <p className="text-sm">
+                      <HomeOutlined className="mr-2" />
+                      {job.company.name}
+                    </p>
+                    <p className="text-sm text-gray-500">
+                      <EnvironmentOutlined className="mr-2" />
+                      {job.location}
+                      <span> · </span>
+                      <span> {moment(job.createdAt).from(now)}</span>
+                    </p>
+                  </div>
+                  <div className="rounded-full border border-gray-200 hover:bg-gray-200">
+                    <PushpinOutlined size="large" className="p-2" />
+                  </div>
                 </div>
-                <div className="rounded-full border border-gray-200 hover:bg-gray-200">
-                  <PushpinOutlined size="large" className="p-2" />
+                <div className="flex justify-start items-center gap-2 mt-3">
+                  <Tag color="purple">{`${job.salary} LPA`}</Tag>
+                  <Tag color="green">{`${job.positions} Positions`}</Tag>
+                  <Tag color="magenta">{`${job.experienceLevel}+ year exp`}</Tag>
                 </div>
-              </div>
-              <div className="flex justify-start items-center gap-2 mt-3">
-                <Tag color="purple">{`${job.salary} LPA`}</Tag>
-                <Tag color="green">{`${job.positions} Positions`}</Tag>
-                <Tag color="magenta">{`${job.experienceLevel}+ year exp`}</Tag>
-              </div>
-            </Card>
-          </List.Item>
-        )}
-      />
+              </Card>
+            </List.Item>
+          )}
+        />
+      )}
     </div>
   );
 };

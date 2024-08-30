5b880097e9f8886ff15ec1d0041fd56dc8c8fa70
Arun Kumar <mymailarun32@gmail.com>
2024-08-30 17:14:27 +0530

    fix(apiCalls/index): remove headers

--- client/src/apiCalls/index.js
+++ client/src/apiCalls/index.js
@@ -2,9 +2,10 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:8080/app/v1",
-  headers: {
-    "Content-Type": "application/json",
-  },
+  // headers: {
+  //   "Content-Type": "application/json",
+  // },
+  withCredentials: true,
 });
 
 export default axiosInstance;

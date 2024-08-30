e63550223be70802ba0c0c4c9565b8ace2d58def
Arun Kumar <mymailarun32@gmail.com>
2024-08-30 17:12:22 +0530

    feat(userRouter): create router for upoloading files in cloudinary using multer

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -4,8 +4,11 @@ const {
   userLogin,
   userLogout,
   updateProfile,
+  uploadProfilePic,
+  getCurrentUser,
 } = require("../controllers/userController");
 const validateJwtToken = require("../middlewares/authentication");
+const multerUploader = require("../userProfile/uploadProfile");
 
 const userRouter = express.Router();
 
@@ -13,5 +16,12 @@ userRouter.post("/signup", userSignup);
 userRouter.post("/login", userLogin);
 userRouter.get("/logout", userLogout);
 userRouter.post("/updateProfile", validateJwtToken, updateProfile);
+userRouter.put(
+  "/uploadProfilePic",
+  multerUploader,
+  validateJwtToken,
+  uploadProfilePic
+);
+userRouter.post("/getUser", validateJwtToken, getCurrentUser);
 
 module.exports = userRouter;

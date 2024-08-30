24152220e3359048085e5bb0425fdbed2ea4424f
Arun Kumar <mymailarun32@gmail.com>
2024-08-30 17:14:02 +0530

    feat(profile): change user profile photo using upload antd component and useMutation userData);

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,18 +1,91 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import Navbar from "../components/Navbar";
-import { Avatar, Badge, Button } from "antd";
+import { Badge, Button, Drawer, Upload, message } from "antd";
 import {
   EditOutlined,
   ContactsOutlined,
   FileDoneOutlined,
+  PlusOutlined,
+  LoadingOutlined,
 } from "@ant-design/icons";
 import AppliedJobTable from "./AppliedJobs";
+import UpdateProfile from "./UpdateProfile";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { getUser, uploadProfilePhoto } from "../apiCalls/user";
 const isResume = true;
 
 const Profile = () => {
   const [open, setOpen] = useState(false);
   const { user } = useSelector((state) => state.users);
+  const [loading, setLoading] = useState(false);
+  const queryClient = useQueryClient();
+
+  const {
+    data: userData,
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: ["user"],
+    queryFn: () => getUser({ userId: user?._id }),
+  });
+
+  const { mutate: uploadPhoto } = useMutation({
+    mutationFn: uploadProfilePhoto,
+    onSuccess: (response) => {
+      queryClient.invalidateQueries({ queryKey: ["user"] });
+      setLoading(false);
+      message.success(response.message);
+    },
+    onError: (error) => {
+      setLoading(false);
+      message.error(error.message);
+    },
+  });
+  const beforeUpload = (file) => {
+    const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
+    if (!isJpgOrPng) {
+      message.error("You can only upload JPG/PNG file!");
+    }
+    const isLt2M = file.size / 1024 / 1024 < 2;
+    if (!isLt2M) {
+      message.error("Image must smaller than 2MB!");
+    }
+    return isJpgOrPng && isLt2M;
+  };
+
+  const handleChange = (info) => {
+    if (info.file.status === "uploading") {
+      const formData = new FormData();
+      formData.append("profilePic", info.file.originFileObj);
+      setLoading(true);
+      uploadPhoto(formData);
+    }
+  };
+
+  const uploadButton = (
+    <button
+      style={{
+        border: 0,
+        background: "none",
+      }}
+      type="button"
+    >
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div
+        style={{
+          marginTop: 4,
+        }}
+      >
+        Profile Picture
+      </div>
+    </button>
+  );
+
+  if (isLoading) return <LoadingOutlined style={{ fontSize: 24 }} />;
+  if (error) return <div>Error: {error.message}</div>;
+
+  console.log(userData);
 
   return (
     <>
@@ -21,14 +94,35 @@ const Profile = () => {
         <div className="max-w-4xl mx-auto bg-white border border-gray-200 rounded-2xl p-5 mt-4">
           <div className="flex justify-between">
             <div className="flex justify-start items-center gap-3">
-              <Avatar
-                className="h-20 w-20"
-                src="https://www.shutterstock.com/image-vector/circle-line-simple-design-logo-600nw-2174926871.jpg"
-                alt="profile"
-              />
+              <div className="flex justify-center items-center">
+                <Upload
+                  listType="picture-circle"
+                  className="avatar-uploader"
+                  showUploadList={false}
+                  beforeUpload={beforeUpload}
+                  onChange={handleChange}
+                >
+                  {userData?.data?.profile?.profilePhoto ? (
+                    <img
+                      src={userData?.data?.profile?.profilePhoto}
+                      alt="profile"
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "cover",
+                        borderRadius: "50%",
+                      }}
+                    />
+                  ) : (
+                    uploadButton
+                  )}
+                </Upload>
+              </div>
               <div>
-                <h1 className="font-medium text-xl">{user?.fullName}</h1>
-                <p>{user?.profile?.bio}</p>
+                <h1 className="font-medium text-xl">
+                  {userData?.data?.fullName}
+                </h1>
+                <p>{userData?.data?.profile?.bio}</p>
               </div>
             </div>
             <Button onClick={() => setOpen(true)} size="small">
@@ -38,18 +132,18 @@ const Profile = () => {
           <div className="my-5">
             <div className="flex items-center gap-3 my-2">
               <FileDoneOutlined />
-              <span>{user?.email}</span>
+              <span>{userData?.data?.email}</span>
             </div>
             <div className="flex items-center gap-3 my-2">
               <ContactsOutlined />
-              <span>{user?.phoneNumber}</span>
+              <span>{userData?.data?.phoneNumber}</span>
             </div>
           </div>
           <div className="my-5">
             <h1>Skills</h1>
             <div className="flex items-center gap-1">
-              {user?.profile?.skills.length !== 0 ? (
-                user?.profile?.skills.map((item, index) => (
+              {userData?.data?.profile?.skills.length !== 0 ? (
+                userData?.data?.profile?.skills.map((item, index) => (
                   <Badge key={index}>{item}</Badge>
                 ))
               ) : (
@@ -62,10 +156,10 @@ const Profile = () => {
             {isResume ? (
               <a
                 target="blank"
-                href={user?.profile?.resume}
+                href={userData?.data?.profile?.resume}
                 className="text-blue-500 w-full hover:underline cursor-pointer"
               >
-                {user?.profile?.resumeOriginalName}
+                {userData?.data?.profile?.resumeOriginalName}
               </a>
             ) : (
               <span>NA</span>
@@ -77,7 +171,15 @@ const Profile = () => {
           <AppliedJobTable />
         </div>
       </div>
-      {/* <UpdateProfileDialog open={open} setOpen={setOpen} /> */}
+      {open && (
+        <Drawer
+          open={open}
+          onClose={() => setOpen(false)}
+          title="Update Profile"
+        >
+          <UpdateProfile setOpen={setOpen} />
+        </Drawer>
+      )}
     </>
   );
 };

1a0736a07cb7e585d332f66d25b95626473967c0
Arun Kumar <mymailarun32@gmail.com>
2024-08-30 17:11:12 +0530

    feat(apiCalls/user): create api calls for user crud operations

---
+++ client/src/apiCalls/user.js
@@ -0,0 +1,33 @@
+import axiosInstance from ".";
+
+export const registerUser = async (payload) => {
+  const response = await axiosInstance.post("/user/signup", payload);
+  return response.data;
+};
+
+export const loginUser = async (payload) => {
+  try {
+    const response = await axiosInstance.post("/user/login", payload);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
+};
+
+export const uploadProfilePhoto = async (payload) => {
+  try {
+    const response = await axiosInstance.put("/user/uploadProfilePic", payload);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
+};
+
+export const getUser = async (payload) => {
+  try {
+    const response = await axiosInstance.post("/user/getUser", payload);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
+};

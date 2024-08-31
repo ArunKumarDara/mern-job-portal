898fe6091577ff6589706015e58eafa52568f8e5
Arun Kumar <mymailarun32@gmail.com>
2024-08-31 16:31:46 +0530

    fix(signup): fix ui bugs

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -24,67 +24,71 @@ const Signup = ({ setSignupDrawer }) => {
   };
 
   return (
-    <Form layout="vertical" onFinish={onFinish}>
-      <Form.Item
-        label="Full Name"
-        name="fullName"
-        rules={[{ required: true, message: "please enter your name" }]}
-      >
-        <Input size="large" />
-      </Form.Item>
-      <Form.Item
-        label="Email"
-        name="email"
-        rules={[{ required: true, message: "please enter your email" }]}
-      >
-        <Input size="large" />
-      </Form.Item>
-      <Form.Item
-        label="Phone Number"
-        name="phoneNumber"
-        rules={[{ required: true, message: "please enter your phone number" }]}
-      >
-        <Input size="large" />
-      </Form.Item>
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[{ required: true, message: "please enter password" }]}
-      >
-        <Input.Password size="large" />
-      </Form.Item>
-      <Form.Item
-        label="Role"
-        name="role"
-        rules={[{ required: true, message: "Please Select your Role" }]}
-      >
-        <Select
-          placement="topRight"
-          size="large"
-          placeholder="Select your Role"
-          options={[
-            {
-              value: "student",
-              label: "Student",
-            },
-            {
-              value: "recruiter",
-              label: "Recruiter",
-            },
+    <>
+      <Form layout="vertical" onFinish={onFinish}>
+        <Form.Item
+          label="Full Name"
+          name="fullName"
+          rules={[{ required: true, message: "please enter your name" }]}
+        >
+          <Input size="large" />
+        </Form.Item>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[{ required: true, message: "please enter your email" }]}
+        >
+          <Input size="large" />
+        </Form.Item>
+        <Form.Item
+          label="Phone Number"
+          name="phoneNumber"
+          rules={[
+            { required: true, message: "please enter your phone number" },
           ]}
-        />
-      </Form.Item>
-      <Form.Item>
-        <div>
-          <button
-            type="submit"
-            className="w-full font-semibold text-white bg-[#6A38C2] rounded-md mb-3 p-3 hover:shadow-md"
-          >
-            SIGN UP
-          </button>
-        </div>
-      </Form.Item>
-    </Form>
+        >
+          <Input size="large" />
+        </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "please enter password" }]}
+        >
+          <Input.Password size="large" />
+        </Form.Item>
+        <Form.Item
+          label="Role"
+          name="role"
+          rules={[{ required: true, message: "Please Select your Role" }]}
+        >
+          <Select
+            placement="topRight"
+            size="large"
+            placeholder="Select your Role"
+            options={[
+              {
+                value: "student",
+                label: "Student",
+              },
+              {
+                value: "recruiter",
+                label: "Recruiter",
+              },
+            ]}
+          />
+        </Form.Item>
+        <Form.Item>
+          <div>
+            <button
+              type="submit"
+              className="w-full font-semibold text-white bg-[#6A38C2] rounded-md mb-3 p-3 hover:shadow-md"
+            >
+              SIGN UP
+            </button>
+          </div>
+        </Form.Item>
+      </Form>
+    </>
   );
 };
 

0123a0dd49cfcb7b0a5c602d7b306d6d4e384c07
Arun Kumar <mymailarun32@gmail.com>
2024-08-31 16:30:25 +0530

    feat(apiCalls/job): create api call for get all jobs and get latest jobs

--- client/src/apiCalls/job.js
+++ client/src/apiCalls/job.js
@@ -2,7 +2,19 @@ import axiosInstance from ".";
 
 export const getLatestJobs = async () => {
   try {
-    const response = await axiosInstance.get("/job/getAllJobs");
+    const response = await axiosInstance.get(`/job/latestJobs?limit=6`);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
+};
+
+export const getJobs = async ({ pageParam }) => {
+  try {
+    console.log(pageParam);
+    const response = await axiosInstance.get(
+      `/job/getAllJobs?page=${pageParam}&limit=10`
+    );
     return response.data;
   } catch (error) {
     return error.response.data;

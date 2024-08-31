3f0c3980f16ef7c02d75f02be04aff3e3af9b048
Arun Kumar <mymailarun32@gmail.com>
2024-08-31 16:27:52 +0530

    feat(jobRouter): create router for get latest jobs

--- server/routes/jobRouter.js
+++ server/routes/jobRouter.js
@@ -4,12 +4,14 @@ const {
   getAllJobs,
   getJobById,
   getAdminJobs,
+  getLatestJobs,
 } = require("../controllers/jobController");
 const validateJwtToken = require("../middlewares/authentication");
 
 const jobRouter = express.Router();
 
 jobRouter.post("/add", validateJwtToken, addJob);
+jobRouter.get("/latestJobs", getLatestJobs);
 jobRouter.get("/getAllJobs", getAllJobs);
 jobRouter.get("/getAdminJobs", validateJwtToken, getAdminJobs);
 jobRouter.get("/:id", getJobById);

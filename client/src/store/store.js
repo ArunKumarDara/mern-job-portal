0fe7eba23c8de6709465e5c51c7b36d3995902a9
Arun Kumar <mymailarun32@gmail.com>
2024-08-31 16:27:20 +0530

    feat(store): configure watchlist reducer in store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "./userSlice";
 import loaderReducer from "./loaderSlice";
+import watchListReducer from "./watchListSlice";
 
 const store = configureStore({
   reducer: {
     users: usersReducer,
     loader: loaderReducer,
+    watchList: watchListReducer,
   },
 });
 

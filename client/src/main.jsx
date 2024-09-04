7e9c425d9d1b11963c4c9b2924fefa0acf58cb95
Arun Kumar <mymailarun32@gmail.com>
2024-09-04 14:42:27 +0530

    feat(main): wrap App with persist gate function

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,18 @@ import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 const queryClient = new QueryClient();
+const persistor = persistStore(store);
 
 createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </QueryClientProvider>
 );

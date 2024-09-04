ffa9accb29db972d8dfa07bd69025d338cbeab57
Arun Kumar <mymailarun32@gmail.com>
2024-09-04 14:41:57 +0530

    feat(store): persist store using redux-persist npm package

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,14 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import usersReducer from "./userSlice";
 import loaderReducer from "./loaderSlice";
 import watchListReducer from "./watchListSlice";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({
+  users: usersReducer,
+  loader: loaderReducer,
+  watchList: watchListReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    users: usersReducer,
-    loader: loaderReducer,
-    watchList: watchListReducer,
-  },
+  reducer: persistedReducer,
 });
 
 export default store;

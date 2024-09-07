730f7b1cc685e82d6f7f6d288f5aefad38819af4
Arun Kumar <mymailarun32@gmail.com>
2024-09-07 09:26:41 +0530

    feat(App): add path ad element for admin companies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Jobs from "./pages/Jobs";
 import Browse from "./pages/Browse";
 import Profile from "./pages/Profile";
 import JobDescription from "./pages/JobDescription";
+import Companies from "./pages/Companies";
 
 const appRouter = createBrowserRouter([
   {
@@ -25,6 +26,10 @@ const appRouter = createBrowserRouter([
   {
     path: "/profile",
     element: <Profile />,
+  },
+  {
+    path: "/admin/companies",
+    element: <Companies />,
   },
 ]);
 

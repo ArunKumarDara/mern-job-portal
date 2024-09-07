d9763cf6deb0462482caf1f891dd6c7837fb6601
Arun Kumar <mymailarun32@gmail.com>
2024-09-07 14:12:42 +0530

    feat(companiesTable): add view , edit buttons and implement their functionalities

--- client/src/components/CompaniesTable.jsx
+++ client/src/components/CompaniesTable.jsx
@@ -1,54 +1,102 @@
-import { Avatar, Space, Table, Typography } from "antd";
+/* eslint-disable react/prop-types */
+import {
+  Avatar,
+  Space,
+  Table,
+  Typography,
+  Spin,
+  Modal,
+  Descriptions,
+  Divider,
+} from "antd";
 import { useQuery } from "@tanstack/react-query";
-import Loading from "../components/Navbar";
 import { getAllCompanies } from "../apiCalls/company";
+import { LoadingOutlined } from "@ant-design/icons";
+import { useState } from "react";
+import moment from "moment";
 
 const { Link } = Typography;
 
-const columns = [
-  {
-    title: "Company Name",
-    dataIndex: "name",
-    key: "name",
-    render: (text, record) => (
-      <div className="flex justify-start items-center">
-        <Avatar src={record.logo} />
-        <h1 className="ml-2 text-sm font-medium">{text}</h1>
-      </div>
-    ),
-  },
-  {
-    title: "Website",
-    dataIndex: "website",
-    key: "website",
-    render: (text) => (
-      <Link href={text} target="_blank">
-        {text}
-      </Link>
-    ),
-  },
-  {
-    title: "Location",
-    dataIndex: "location",
-    key: "location",
-  },
-  {
-    title: "Employees",
-    dataIndex: "employees",
-    key: "employees",
-  },
-  {
-    title: "Action",
-    key: "action",
-    render: () => (
-      <Space size="middle">
-        <a>View</a>
-        <a>Edit</a>
-      </Space>
-    ),
-  },
-];
-const CompaniesTable = () => {
+const CompaniesTable = ({
+  setOpenDrawer,
+  setSelectedCompany,
+  selectedCompany,
+}) => {
+  const [view, setView] = useState(false);
+
+  const columns = [
+    {
+      title: "Company Name",
+      dataIndex: "name",
+      key: "name",
+      render: (text, record) => (
+        <div className="flex justify-start items-center">
+          <Avatar src={record.logo} />
+          <h1 className="ml-2 text-sm font-medium">{text}</h1>
+        </div>
+      ),
+    },
+    {
+      title: "Website",
+      dataIndex: "website",
+      key: "website",
+      render: (text) => (
+        <Link href={text} target="_blank">
+          {text}
+        </Link>
+      ),
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (record) => (
+        <Space size="middle">
+          <a
+            onClick={() => {
+              setView(true), setSelectedCompany(record);
+            }}
+          >
+            View
+          </a>
+          <a
+            onClick={() => {
+              setOpenDrawer(true), setSelectedCompany(record);
+            }}
+          >
+            Edit
+          </a>
+        </Space>
+      ),
+    },
+  ];
+
+  const items = [
+    {
+      key: "1",
+      label: "Website",
+      children: (
+        <Link href={selectedCompany.website} target="_blank">
+          {selectedCompany.website}
+        </Link>
+      ),
+    },
+    {
+      key: "2",
+      label: "Location",
+      children: selectedCompany.location,
+    },
+    {
+      key: "3",
+      label: "Employees",
+      children: selectedCompany.employees,
+    },
+    {
+      key: "4",
+      label: "Created At",
+      children: moment(selectedCompany.createdAt).format("YYYY-MM-DD"),
+    },
+  ];
+
   const {
     data: companies,
     isLoading,
@@ -63,9 +111,30 @@ const CompaniesTable = () => {
   return (
     <div>
       {isLoading ? (
-        <Loading />
+        <div className="flex justify-center items-center">
+          <Spin indicator={<LoadingOutlined spin />} />
+        </div>
       ) : (
         <Table columns={columns} dataSource={companies.data} />
+      )}
+      {view && (
+        <Modal open={view} footer={null} onCancel={() => setView(false)}>
+          <div>
+            <div className="flex flex-col justify-start items-start gap-2">
+              <div className="flex justify-start items-center gap-2">
+                <Avatar src={selectedCompany.logo} size="large" />
+                <h1 className="font-semibold text-lg">
+                  {selectedCompany.name}
+                </h1>
+              </div>
+              <p className="text-sm text-gray-500">
+                {selectedCompany.description}
+              </p>
+            </div>
+            <Divider />
+            <Descriptions items={items} column={1} />
+          </div>
+        </Modal>
       )}
     </div>
   );

0a8e23a810872bf6eeea96fe59e73789cbfb7f01
Arun Kumar <mymailarun32@gmail.com>
2024-09-07 09:12:11 +0530

    feat(apiCall/company): create api call for get and create companies

--- client/src/apiCalls/company.js
+++ client/src/apiCalls/company.js
@@ -2,7 +2,20 @@ import axiosInstance from ".";
 
 export const getAllCompanies = async () => {
   try {
-    const response = await axiosInstance.get("/company/getAllCompanies");
+    const response = await axiosInstance.get("/company/getCompaniesByUser");
+    return response.data;
+  } catch (error) {
+    return error.message;
+  }
+};
+
+export const addCompany = async (payload) => {
+  try {
+    const response = await axiosInstance.post("/company/register", payload, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
     return response.data;
   } catch (error) {
     return error.message;

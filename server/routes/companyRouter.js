2e742a23edfabe3be8f2bed7b2533915c9f437dc
Arun Kumar <mymailarun32@gmail.com>
2024-09-07 09:14:58 +0530

    feat(companyRouter): create router for add company and middleware for upload logo using multer

--- server/routes/companyRouter.js
+++ server/routes/companyRouter.js
@@ -7,9 +7,15 @@ const {
   getCompaniesByUser,
 } = require("../controllers/companyController");
 const validateJwtToken = require("../middlewares/authentication");
+const multerLogoUploader = require("../company/LogoUploader");
 const companyRouter = express.Router();
 
-companyRouter.post("/register", validateJwtToken, registerCompany);
+companyRouter.post(
+  "/register",
+  multerLogoUploader,
+  validateJwtToken,
+  registerCompany
+);
 companyRouter.get("/getAllCompanies", validateJwtToken, getAllCompanies);
 companyRouter.get("/getCompaniesByUser", validateJwtToken, getCompaniesByUser);
 companyRouter.get("/:id", validateJwtToken, getCompanyById);

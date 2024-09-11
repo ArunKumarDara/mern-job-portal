32d84fc74b9a7df611dd6ce40b888f5fbd04ab30
Arun Kumar <mymailarun32@gmail.com>
2024-09-11 22:14:54 +0530

    feat(userRouter): add multer uploader middleware

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -9,13 +9,19 @@ const {
 } = require("../controllers/userController");
 const validateJwtToken = require("../middlewares/authentication");
 const multerUserProfileUploader = require("../userProfile/uploadProfile");
+const multerUserResumeUploader = require("../userProfile/uploadResume");
 
 const userRouter = express.Router();
 
 userRouter.post("/signup", userSignup);
 userRouter.post("/login", userLogin);
 userRouter.get("/logout", userLogout);
-userRouter.post("/updateProfile", validateJwtToken, updateProfile);
+userRouter.post(
+  "/updateProfile",
+  multerUserResumeUploader,
+  validateJwtToken,
+  updateProfile
+);
 userRouter.put(
   "/uploadProfilePic",
   multerUserProfileUploader,

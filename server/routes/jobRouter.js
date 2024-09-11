6afcd9c268ea1f4f317dcc371e592ac5f5caf07a
Arun Kumar <mymailarun32@gmail.com>
2024-09-11 09:48:27 +0530

    feat(jobRouter): create router for updating job

--- server/routes/jobRouter.js
+++ server/routes/jobRouter.js
@@ -5,6 +5,7 @@ const {
   getJobById,
   getAdminJobs,
   getLatestJobs,
+  updateJob,
 } = require("../controllers/jobController");
 const validateJwtToken = require("../middlewares/authentication");
 
@@ -15,5 +16,6 @@ jobRouter.get("/latestJobs", getLatestJobs);
 jobRouter.get("/getAllJobs", getAllJobs);
 jobRouter.get("/getAdminJobs", validateJwtToken, getAdminJobs);
 jobRouter.get("/:id", getJobById);
+jobRouter.post("/update/:id", validateJwtToken, updateJob);
 
 module.exports = jobRouter;
